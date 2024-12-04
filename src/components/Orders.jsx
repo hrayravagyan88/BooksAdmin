644c929487f84132277f4db09c1abbec0a63e1a1
Hrayr Avagyan <hrayravagyan.am@gmail.com>
2024-12-04 15:34:39 +0400

    styles

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -15,9 +15,9 @@ const Profile = () => {
       try {
         const querySnapshot = await getDocs(collection(db, collectionName));
         const updatedOrders = [];
-        
+
         // Use a for loop instead of map
-        
+
         for (const orderDoc of querySnapshot.docs) {
           const orderData = orderDoc.data();
           const bookId = orderData.doc_id;
@@ -35,7 +35,7 @@ const Profile = () => {
             updatedOrders.push({ ...orderData, bookTitle: "No Book Assigned" });
           }
         }
-        
+
         setData(updatedOrders);
       } catch (error) {
       } finally {
@@ -63,7 +63,7 @@ const Profile = () => {
       "Are you sure you want to delete this user?"
     );
     if (confirmDelete) {
-     // await deleteDoc(doc(db, "users", userId)); // Replace "users" with your collection name
+      // await deleteDoc(doc(db, "users", userId)); // Replace "users" with your collection name
       //fetchUsers(); // Refresh the users list
     }
   };
@@ -76,106 +76,110 @@ const Profile = () => {
     <div className="container mx-auto p-4 pl-0">
       <h1 className="text-2xl font-bold text-center mb-8">Orders</h1>
       <div className="overflow-auto">
-      {data.length === 0 ? (
-        <div>No data available</div>
-      ) : (
-        <table className=" overflow-auto table-auto w-full border-collapse border border-gray-200">
-          <thead className="bg-gray-100">
-            <tr className="text-xs">
-              
-              <th className="border border-gray-300 px-4 py-2">BookName</th>
-              <th className="border border-gray-300 px-4 py-2">Address</th>
-              <th className="border border-gray-300 px-4 py-2">City</th>
-              <th className="border border-gray-300 px-4 py-2">DeLivery</th>
-              <th className="border border-gray-300 px-4 py-2">Mail</th>
-              <th className="border border-gray-300 px-4 py-2">FullName</th>
-              <th className="border border-gray-300 px-4 py-2">Note</th>
-              <th className="border border-gray-300 px-4 py-2">Phone</th>
-              <th colSpan='4' className="border border-gray-300 px-4 py-2">Images</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => {
-              const mediaEntries = Object.entries(item).filter(([key, value]) => key.startsWith('media'));
-              return (<tr className="text-xs" key={item.id}>
-                <td className="border border-gray-300 px-4 py-2">{item.bookTitle}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.address}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.city || "N/A"}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.delivery? "Yes" :"NO"}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.mail || "N/A"}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.fullName || "N/A"}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.note || "N/A"}</td>
-                <td className="border border-gray-300 px-4 py-2">{item.phone || "N/A"}</td>
-                <td className="border border-gray-300 px-4 py-2">
-                  <div className="grid grid-cols-2 gap-2">   {mediaEntries.map(([key, value]) => (
+        {data.length === 0 ? (
+          <div>No data available</div>
+        ) : (
+          <table className=" overflow-auto table-auto w-full border-collapse border border-gray-200">
+            <thead className="bg-gray-100">
+              <tr className="text-xs">
 
-                    <img
-                      key={key}
-                      src={value}
-                      alt={`UserImage ${key + 1}`}
-                      className="cursor-pointer w-16 h-16 object-cover rounded"
-                      onClick={() => setSelectedImage(value)}
-                    />
+                <th className="border border-gray-300 px-4 py-2">BookName</th>
+                <th className="border border-gray-300 px-4 py-2">Address</th>
+                <th className="border border-gray-300 px-4 py-2">City</th>
+                <th className="border border-gray-300 px-4 py-2">DeLivery</th>
+                <th className="border border-gray-300 px-4 py-2">Mail</th>
+                <th className="border border-gray-300 px-4 py-2">FullName</th>
+                <th className="border border-gray-300 px-4 py-2">Note</th>
+                <th className="border border-gray-300 px-4 py-2">Phone</th>
+                <th className="border border-gray-300 px-4 py-2">Images</th>
+                <th  className="border border-gray-300 px-4 py-2">Action</th>
+               <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600">
+                  +Add
+                </button>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => {
+                const mediaEntries = Object.entries(item).filter(([key, value]) => key.startsWith('media'));
+                return (<tr className="text-xs" key={item.id}>
+                  <td className="border border-gray-300 px-4 py-2">{item.bookTitle}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.address}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.city || "N/A"}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.delivery ? "Yes" : "NO"}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.mail || "N/A"}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.fullName || "N/A"}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.note || "N/A"}</td>
+                  <td className="border border-gray-300 px-4 py-2">{item.phone || "N/A"}</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    <div className="grid grid-cols-2 gap-2">   {mediaEntries.map(([key, value]) => (
 
-                  ))} </div>
-                </td>
-                {editingOrder === item.doc_id ? (
-                  <>
-                    <button
-                      className="bg-blue-500 text-white px-4 py-1 rounded"
-                      onClick={() => handleSave(item.id)}
-                    >
-                      Save
-                    </button>
-                    <button
-                      className="bg-red-500 text-white px-4 py-1 rounded ml-2"
-                      onClick={handleCancel}
-                    >
-                      Cancel
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <button
-                      className="bg-yellow-500 text-white px-4 py-1 rounded mr-2"
-                      onClick={() => handleEdit(item.doc_id)}
-                    >
-                      Edit
-                    </button>
-                  <button
-                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-                    onClick={() => handleDelete(item.id)}
-                  >
-                    Delete
-                  </button>
-                  </>
-                )}
-               
-              </tr>)
-            })}
-          </tbody>
-        </table>
-      )}
-      {selectedImage && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-          onClick={() => setSelectedImage(null)} // Close modal on click
-        >
-          <div className="relative">
-            <img
-              src={selectedImage}
-              alt="Full Size"
-              className="max-w-full max-h-screen rounded shadow-lg"
-            />
-            <button
-              className="absolute top-2 right-2 bg-red-500 text-white p-2 rounded-full"
-              onClick={() => setSelectedImage(null)}
-            >
-              ✕
-            </button>
+                      <img
+                        key={key}
+                        src={value}
+                        alt={`UserImage ${key + 1}`}
+                        className="cursor-pointer w-16 h-16 object-cover rounded"
+                        onClick={() => setSelectedImage(value)}
+                      />
+
+                    ))} </div>
+                  </td>
+                  {editingOrder === item.doc_id ? (
+                    <td  className="border border-gray-300 px-4 py-2">
+                      <button
+                        className="bg-blue-500 text-white px-4 py-1 rounded"
+                        onClick={() => handleSave(item.id)}
+                      >
+                        Save
+                      </button>
+                      <button
+                        className="bg-red-500 text-white px-4 py-1 rounded ml-2"
+                        onClick={handleCancel}
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  ) : (
+                    <td className="border border-gray-300 px-4 py-2">
+                      <button
+                        className="bg-yellow-500 text-white px-4 py-1 rounded mr-2"
+                        onClick={() => handleEdit(item.doc_id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  )}
+
+                </tr>)
+              })}
+            </tbody>
+          </table>
+        )}
+        {selectedImage && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+            onClick={() => setSelectedImage(null)} // Close modal on click
+          >
+            <div className="relative">
+              <img
+                src={selectedImage}
+                alt="Full Size"
+                className="max-w-full max-h-screen rounded shadow-lg"
+              />
+              <button
+                className="absolute top-2 right-2 bg-red-500 text-white p-2 rounded-full"
+                onClick={() => setSelectedImage(null)}
+              >
+                ✕
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </div>
   );
